e39196af3741ea9be4576fe7710b1e2aabb2e0d0
Sal Espinosa <sespinos@gmail.com>
2016-10-04 16:45:53 -0600

    Update based on comments from Casey

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,13 +4,7 @@ require File.expand_path('../../config/environment.rb', __FILE__)
 
 ENV['RACK_ENV'] = 'test'
 
-module RSpecMixin
-  include Rack::Test::Methods
-  def app() described_class end
-end
-
 RSpec.configure do |c|
-  c.include RSpecMixin
   c.before(:all) do
     Task.destroy_all
   end
